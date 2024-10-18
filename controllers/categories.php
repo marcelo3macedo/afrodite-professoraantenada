1b9d641daa7eadfcdcbfc2991f9a38bf083f1565
Marcelo Macedo <marcelo3macedo@gmail.com>
2024-10-18 19:55:34 -0300

    feat: adding caching

--- controllers/categories.php
+++ controllers/categories.php
@@ -6,22 +6,26 @@ include_once __DIR__ . '/../helpers/posts.php';
 
 function get_categories_data()
 {
-    $categories = get_categories_with_children();
+    return get_or_set_transient(__FUNCTION__, function () {
+        $categories = get_categories_with_children();
 
-    return [
-        'categories' => $categories
-    ];
+        return [
+            'categories' => $categories
+        ];
+    });
 }
 
 function get_category_data($categoryId, $pageId = 1)
 {
-    $category = get_queried_object();
-    $posts = get_posts_by_category($categoryId, $pageId);
-    $subcategories = get_subcategories($categoryId);
+    return get_or_set_transient(__FUNCTION__ . "_" . $categoryId . "_" . $pageId, function () use ($categoryId, $pageId) {
+        $category = get_queried_object();
+        $posts = get_posts_by_category($categoryId, $pageId);
+        $subcategories = get_subcategories($categoryId);
 
-    return [
-        'posts' => $posts,
-        'category' => $category,
-        'subcategories' => $subcategories
-    ];
+        return [
+            'posts' => $posts,
+            'category' => $category,
+            'subcategories' => $subcategories
+        ];
+    });
 }
