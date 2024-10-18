aced0c1d4e6d81a453b1d8a6e6dd2f8d1a8fc438
Marcelo Macedo <marcelo3macedo@gmail.com>
2024-10-18 16:11:27 -0300

    feat: institutional pages

--- page-duvidas-e-sugestoes.php
+++ page-duvidas-e-sugestoes.php
@@ -1,40 +1,48 @@
 <?php
-	global $lang;
+global $lang;
 
-	$json = file_get_contents(get_template_directory() . '/assets/translations/pt.json');
-	$lang = json_decode($json);
-	$socialJson = file_get_contents(get_template_directory() . '/assets/data/social.json');
-    $social_items = json_decode($socialJson);
+$json = file_get_contents(get_template_directory() . '/assets/translations/pt.json');
+$lang = json_decode($json);
+$socialJson = file_get_contents(get_template_directory() . '/assets/data/social.json');
+$social_items = json_decode($socialJson);
 
-	include_once 'constants/loader.php';
-	include_once 'components/header/main.php';
-	include_once 'components/menu/main.php';
-	include_once 'helpers/content.php';
-    include_once 'elements/gallery/social.php';
+include_once 'constants/loader.php';
+include_once 'components/header/main.php';
+include_once 'components/menu/main.php';
+include_once 'helpers/content.php';
+include_once 'elements/gallery/social.php';
 ?>
 
-<div class="page container">
-	<div class="block">
-		<div class="title">
-			<?php getTextMultiLineContent('h1', $lang->help->title) ?>
+<div class="container mx-auto p-4 min-h-lvh md:flex w-full">
+	<section class="bg-yellow-50 p-4 rounded-xl w-full">
+		<h1 class="text-2xl font-bold py-2 text-violet-950"><?= $lang->help->title; ?></h1>
+		<div class="my-2">
+			<p class="text-lg"><?= $lang->help->description; ?></p>
 		</div>
-		<div class="body">
-			<?php getTextMultiLineContent('h3', $lang->help->description) ?>
+		<div class="my-4">
+			<div class="font-bold">
+				<?php getTextMultiLineContent('h3', $lang->help->contact->title) ?>
+			</div>
+			<?php getTextMultiLineContent('p', $lang->help->contact->description) ?>
 		</div>
-	</div>
-	<div class="block">
-		<div class="title">
-			<?php getTextMultiLineContent('h2', $lang->help->contact->title) ?>
+		<div class="my-4 flex">
+			<?php if (is_array($social_items)): ?>
+				<?php foreach ($social_items as $item): ?>
+					<div class="item">
+						<a href="<?= $item->link; ?>">
+							<div class="container noselect">
+								<div class="image">
+									<img class="w-20 px-4" src="<?= get_template_directory_uri() . '/' . $item->thumbnail; ?>" alt="<?= $item->title; ?>">
+								</div>
+							</div>
+						</a>
+					</div>
+				<?php endforeach; ?>
+			<?php endif; ?>
 		</div>
-		<div class="body">
-			<?php getTextMultiLineContent('h3', $lang->help->contact->description) ?>
-		</div>
-	</div>
-	<div class="block social">
-		<?php load_social_gallery($social_items) ?>
-	</div>
+	</section>
 </div>
 
 <?php
-	include_once 'components/footer/main.php';
+get_template_part("views/footer/bar");
 ?>
