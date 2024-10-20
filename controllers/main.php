c60cfa3233ac16f0056656a1ab2fa2c864c95031
Marcelo Macedo <marcelo3macedo@gmail.com>
2024-10-20 11:23:07 -0300

    feat: add caching

--- controllers/main.php
+++ controllers/main.php
@@ -5,16 +5,16 @@ include_once __DIR__ . '/../helpers/posts.php';
 
 function get_main_data($page)
 {
-    //return get_or_set_transient(__FUNCTION__ . "_" . $page, function () use ($page) {
-    $main_menu_id = get_theme_mod('main_categories');
-    $categories = get_all_categories_with_children($main_menu_id);
-    $posts = get_latest_posts($page);
+    return get_or_set_transient(__FUNCTION__ . "_" . $page, function () use ($page) {
+        $main_menu_id = get_theme_mod('main_categories');
+        $categories = get_all_categories_with_children($main_menu_id);
+        $posts = get_latest_posts($page);
 
-    return [
-        'postTitle' => 'Novidades',
-        'categoryTitle' => 'Categorias',
-        'categories' => $categories,
-        'posts' => $posts
-    ];
-    //});
+        return [
+            'postTitle' => 'Novidades',
+            'categoryTitle' => 'Categorias',
+            'categories' => $categories,
+            'posts' => $posts
+        ];
+    });
 }
